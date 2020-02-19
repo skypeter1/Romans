4ca2f4265310c17f108e5a8148e13e4a4a38f420
skypeter1 <skypeter1@hotmail.com>
2020-02-19 21:01:50 +0100

    Add test folder

---
+++ tests/spec.js
@@ -0,0 +1,11 @@
+var request = require('supertest');
+var myApp = require('../app');
+var assert = require('assert');
+
+var numbers = [0,1]
+
+describe('App module Smoke Test', function() {
+    // console.log(myApp.romans);
+    // assert.equal(typeof myApp.romans, 'function','SHould be a function that converts');
+    //console.log("test");
+})
